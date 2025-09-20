0b9f5a2deee37ad25bfad0523fa33393b65f1e68
Arunanshu Dey <arunanshudey7@gmail.com>
2025-09-20 09:24:12 +0530

    add : contract added

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,8 +1,56 @@
-import { Wheat, HomeIcon, Wallet, LucideLayoutDashboard, LucideLanguages, LineChart , User} from 'lucide-react';
+import { Wheat,LogOut, HomeIcon, Wallet, LucideLayoutDashboard, LucideLanguages, LineChart , User} from 'lucide-react';
 import { Link, useLocation } from 'react-router-dom';
+import { useWallet } from '../contexts/WalletContext';
+import {useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate()
   const location = useLocation();
+    const { 
+    account, 
+    contract, 
+    first,
+    connectWallet, 
+    handleDisconnect, 
+    initializeContract,
+    balance, 
+    isConnected,
+    setBalance
+  } = useWallet();
+
+    useEffect(() => {
+      if(contract){
+        console.log(" Here is contract : ", contract)
+        console.log("account : ", account)
+        // if(!first) navigate('/signup')
+      }
+      
+
+    }, [contract,first])
+   const handleWalletClick = async () => {
+  if (isConnected) {
+    handleDisconnect();
+    // toast.info("Wallet disconnected");
+  } else {
+    try {
+      await connectWallet();
+      await initializeContract()
+        
+     
+      
+      // toast.success("Metamask connected successfully!");
+    } catch (error) {
+      // toast.error("Failed to connect wallet ❌");
+      console.log("error : ", error)
+    }
+  }
+};
+
+  const formatAccount = (addr) => {
+    if (!addr) return "";
+    return `${addr.slice(0, 6)}...${addr.slice(-4)}`;
+  };
 
   return (
     <header className="bg-white shadow-sm border-b border-gray-200 fixed top-0 left-0 right-0 z-50">
@@ -25,9 +73,18 @@ const Navbar = () => {
               <LucideLayoutDashboard className="w-5 h-5" />
               <span className="text-sm">Dashboard</span>
             </Link>
-            <div className="flex items-center space-x-2 text-gray-700 cursor-pointer hover:text-green-800">
-              <Wallet className="w-5 h-5" />
-              <span className="text-sm">Connect Wallet</span>
+            <div onClick= {handleWalletClick} className="flex items-center space-x-2 text-gray-700 cursor-pointer hover:text-green-800">
+              {isConnected ? (
+              <>
+                <LogOut size={18} />
+                <span>{formatAccount(account)}</span>
+              </>
+            ) : (
+              <>
+                <Wallet size={18} />
+                <span>Connect Wallet</span>
+              </>
+            )}
             </div>
             <div className="flex items-center space-x-2 text-gray-700 cursor-pointer hover:text-green-800">
               <LucideLanguages className="w-5 h-5" />
